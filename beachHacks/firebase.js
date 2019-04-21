b3eae9ca3f9f79eb1ef459281886283ef09a2af7
rahulb124 <rahulbekal24@gmail.com>
2019-04-21 12:35:17 -0700

    x and y change

--- beachHacks/firebase.js
+++ beachHacks/firebase.js
@@ -64,7 +64,7 @@ function draw(user, count, total, itemType) {
 		ctx.fillText(count, 300, 120);
 		// Only draw this total count if not trash
 		if (itemType === "recycle" || itemType === "compost") {
-			ctx.fillText(total, 35, 55);
+			ctx.fillText(total, 32, 75);
 		}
 		var dataURL = output();
 		updateURL(user, dataURL);
